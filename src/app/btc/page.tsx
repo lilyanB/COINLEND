8e436ef9b1cb09d24e76dd18baf6d2f45d1f2fa0
lilyanB <lilyan.bastien@gmail.com>
2023-08-24 17:04:59 +0200

    init

--- src/app/btc/page.tsx
+++ src/app/btc/page.tsx
@@ -1,12 +1,12 @@
-import Header from "./components/Header";
+import Header from "../components/Header";
 
-export default function Home() {
+export default function btc() {
   return (
     <div className="body bg-[#111524] flex flex-col relative">
       <main>
         <Header />
-        home
       </main>
+      btc
     </div>
   );
 }
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <div className="p-4 flex items-center justify-between">
+      <div className="flex items-center">
+        <Link href={"/"}>
+          <Image
+            src={"/logo.png"}
+            width={40}
+            height={40}
+            className="mr-12"
+            alt={""}
+          />
+        </Link>
+        {/* links Div  */}
+
+        <ul className="flex flex-grow justify-around">
+          <li className="li">
+            <Link href={"/btc"}>BTC</Link>
+          </li>
+          <li className="li">
+            <Link href={"/lendborrow"}>Lending and Borring</Link>
+          </li>
+          <li className="li">
+            <Link href={"/wbtc"}>wBTC</Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* last div  */}
+      <div className="flex items-center flex space-x-4">connect</div>
+    </div>
+  );
+};
+
+export default Header;
