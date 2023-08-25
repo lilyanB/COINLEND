de0c0f6f50cec39cc1bdfa564b74074b0e92bc7d
lilyanB <lilyan.bastien@gmail.com>
2023-08-25 18:21:24 +0200

    fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Header from "./components/Header";
 
+declare global {
+  interface Window {
+    xfi: any; // Replace 'any' with the actual type of 'xfi' if you know it
+  }
+}
+
 export default function Home() {
   return (
     <div className="body bg-[#111524] flex flex-col relative">
