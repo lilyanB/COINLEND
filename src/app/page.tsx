f7099f26895bb28d2b2c638cb08a4472eebbb609
lilyanB <lilyan.bastien@gmail.com>
2023-09-08 14:44:58 +0200

    rework css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ interface LinkButtonProps {
 function LinkButton({ href, children }: LinkButtonProps) {
   return (
     <Link href={href}>
-      <div className="py-7 px-24 shadow-2xl font-bold shadow-black bg-white hover:bg-amber-600 rounded-lg border-4 border-black">
+      <div className="py-7 px-24 text-lg shadow-2xl font-bold shadow-black bg-white hover:bg-amber-600 rounded-lg border-4 border-black">
         {children}
       </div>
     </Link>
@@ -28,9 +28,11 @@ export default function Home() {
     <main>
       <div className="flex flex-col justify-center items-center h-screen">
         <div className="flex flex-col items-center space-y-40">
-          <div className="py-7 px-24 bg-white rounded-lg shadow-2xl border-4 border-black text-center">
-            <h1 className="text-4xl font-bold">BitLend</h1>
-            <p>Lending protocol on Bitcoin</p>
+          <div className="pt-7 bg-white rounded-md shadow-2xl border-x-2 border-t-2 border-b-1 border-black text-center">
+            <div className="py-7 px-24 bg-white rounded-md shadow-2xl border-2 border-black text-center">
+              <h1 className="pb-4 text-4xl font-bold">BitLend</h1>
+              <p className="text-sm italic">Lending protocol on Bitcoin</p>
+            </div>
           </div>
           <div className="flex flex-row items-center space-x-16">
             {/* Liens */}
